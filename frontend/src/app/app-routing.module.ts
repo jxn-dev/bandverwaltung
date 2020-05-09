81cb33bdb22081920b6dc731a6d5c4a8557fa05b
jx4n16 <jan.eggenberger01@gmail.com>
2020-05-09 10:41:26 +0200

    routing added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SongsComponent } from 'src/app/components/songs/songs.component';
+import { GigsComponent } from 'src/app/components/gigs/gigs.component';
+import { PhotosComponent } from 'src/app/components/photos/photos.component';
+import { AppComponent } from './app.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'songs', component: SongsComponent},
+  {path: 'gigs', component: GigsComponent},
+  {path: 'photos', component: PhotosComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { SongsComponent } from './components/songs/songs.component';
 import { GigsComponent } from './components/gigs/gigs.component';
 import { PhotosComponent } from './components/photos/photos.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { PhotosComponent } from './components/photos/photos.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
